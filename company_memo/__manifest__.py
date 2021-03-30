bc13d39eceef229a99be059d27e107e06e57baad
christopher <mchristopher@vasconsolutions.com>
2021-03-30 19:11:08 +0100

    [FIX] Added thumbnail to v10.0

--- company_memo/__manifest__.py
+++ company_memo/__manifest__.py
@@ -19,7 +19,7 @@
         'views/assets.xml',
         'security/ir.model.access.csv'
     ],
-    "images": ['images/office_memo.png'],
+    "images": ['images/office_memo.jpg'],
     # 'qweb': [
     #     'static/src/xml/base.xml',
     # ],
